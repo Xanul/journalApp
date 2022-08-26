d37f01c906cfe021352d51321ce1bf6521bf319c
Xanul <rivas.060@gmail.com>
2022-08-26 09:57:03 -0500

    Added the JournalLayout with Pages and Views

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,33 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView, NoteView } from "../views"
+
+
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea hic autem, et quis voluptatem reprehenderit officia neque nemo pariatur blanditiis magnam itaque expedita, nobis temporibus exercitationem officiis atque soluta.</Typography> */}
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.8 },
+          position: "fixed",
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize: 30}}/>
+      </IconButton>
+      
+
+    </JournalLayout>
   )
 }
