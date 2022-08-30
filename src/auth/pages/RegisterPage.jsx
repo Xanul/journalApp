4fb9ecf935cf33dc22baf9c65a2ec2a46cce5a3a
Xanul <rivas.060@gmail.com>
2022-08-30 10:09:05 -0500

    Added Redux with basic config

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -47,7 +47,7 @@ export const RegisterPage = () => {
             </Grid>
             <Grid container direction="row" justifyContent="end">
               <Typography sx={{mr: 1}}>¿Ya tienes cuenta?</Typography>
-              <Link component={ RouterLink } color="inherit" to="/auth/register">
+              <Link component={ RouterLink } color="inherit" to="/auth/login">
                 Ingresar
               </Link>
             </Grid>
